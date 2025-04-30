7091b8d3ada74caf83d5c2612b6734454f12a742
sumanrahul <rahulkumarsuman389@gmailcom>
2025-05-01 01:00:18 +0530

    setting up of react app , adding tailwind css and testing it

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-red-600">App</h1>
+      <h1 className="text-green-700">React</h1>
+    </div>
+  )
+}
+
+export default App
